ce9957c1e4f8e6ba01a89af6215bd64a694ee0fe
ZulemaO <95653244+ZulemaO@users.noreply.github.com>
2021-12-13 10:48:09 -0600

    final

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
     clean()
     drawName()
 
-    //Agregar.
+//Agregar.
     $('#agregarBoton').click(function(){
         //campo-nombre
         if($('#nombre').val() == '' || $('#nombre').val() == null || $('#nombre').val().length == 0){
@@ -111,6 +111,7 @@ $(document).ready(function(){
         localStorage.setItem('person',JSON.stringify(del))
         drawName()
     })
+
     //Editar
     $(document.body).on('click','.edit_btn',function(){
         console.log(this.value)
@@ -119,13 +120,73 @@ $(document).ready(function(){
         edit=JSON.parse(edit)
         console.log(edit)
 
-        edit = edit.filter(e => e.Email != this.value)
-
+        edit = edit.filter(e => e.Email == this.value)
         console.log(edit)
+        
 
-        localStorage.setItem('person',JSON.stringify(edit))
+        for(let i of edit){
+            console.log(i)
+            console.log(i.Age)
+            $('#nombre').val(i.Name)
+            $('#apellido').val(i.LastN)
+            $('#edad').val(i.Age)
+            $('#calle').val(i.Street)
+            $('#numero').val(i.Number)
+            $('#colonia').val(i.Col)
+            $('#correo').val(i.Email)
+            $('#telefono').val(i.Cel)
+            $('#fecha').val(i.Date)
+        }
+        $('#EditarButton').val(this.value)
         drawName()
     })
+
+//Función de Editar.
+    $('#EditarButton').click(function(){
+        //campo-nombre
+        if($('#nombre').val() == '' || $('#nombre').val() == null || $('#nombre').val().length == 0){
+            alert('Favor de llenar los campos faltantes.') 
+            return 0
+        }
+        //campo-edad
+        if($('#edad').val() == '' || $('#edad').val() == null || $('#edad').val() <= 17){
+            alert('Para continuar usted debe ser mayor de edad.') 
+            return 0
+        }
+        //campo-email
+        const array1 = ['@', '.']
+        console.log(array1.includes('@', '.'))
+        if($('#correo').val() == '' || $('#correo').val() == null || $('#correo').val().includes('@') == false || $('#correo').val().includes('.') == false){
+            alert('El correo es incorrecto, favor de ingresarlo correctamente.') 
+            return 0
+        }
+        
+        let edit = localStorage.getItem('person')
+        edit=JSON.parse(edit)
+        console.log(edit)
+
+        for(let i of edit){
+            if(this.value == i.Email){
+                i.Name = $('#nombre').val()
+                i.LastN = $('#apellido').val()
+                i.Age = $('#edad').val()
+                i.Street = $('#calle').val()
+                i.Number= $('#numero').val()
+                i.Col= $('#colonia').val()
+                i.Email = $('#correo').val()
+                i.Cel = $('#telefono').val()
+                i.Date = $('#fecha').val()
+    
+                console.log(i)
+            }
+        }
+
+        localStorage.setItem('person', JSON.stringify(edit))
+
+        drawName()
+        clean()
+    })
+
 
     
 })
